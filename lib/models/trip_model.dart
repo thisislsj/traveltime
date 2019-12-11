76bc55bb3125ba592555fdbfeb871e7b6632fa3c
Lahiru Sandeepa <lahirusandeepa@outlook.com>
2019-12-12 00:16:56 +0530

    Trip Sample Data

--- lib/models/trip_model.dart
+++ lib/models/trip_model.dart
@@ -1,7 +1,30 @@
 
 import 'package:traveltime/models/activity_model.dart';
+import 'package:traveltime/models/destination_model.dart';
 
 class Trip{
   String tripName;
   List<Activity> tripActivities;
+
+  Trip({
+    this.tripName,
+    this.tripActivities,
+  });
 }
+
+List<Trip> trips=[
+  Trip(
+    tripName: 'Ella1',
+    tripActivities: activities,
+  ),
+
+  Trip(
+    tripName: 'Kandy1',
+    tripActivities: activities,
+  ),
+
+  Trip(
+    tripName: 'Galle1',
+    tripActivities: activities,
+  ),
+];
