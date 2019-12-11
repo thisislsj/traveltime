4d1c3b9d8d810a8ce2a4d3de0aa52c824ce563e2
Lahiru Sandeepa <lahirusandeepa@outlook.com>
2019-12-12 02:16:39 +0530

    Destination Carousel

---
+++ lib/screens/destination_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:traveltime/models/destination_model.dart';
+
+class DestinationScreen extends StatefulWidget{
+  final Destination destination;
+  DestinationScreen({this.destination});
+
+  @override  
+  _DestinationScreenState createState()=> _DestinationScreenState();
+}
+
+class _DestinationScreenState extends State<DestinationScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }  
+  
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:traveltime/widgets/destination_carousal.dart';
 
 class HomeScreen extends StatefulWidget{
   @override  
@@ -8,8 +9,31 @@ class HomeScreen extends StatefulWidget{
 class _HomeScreenState extends State<HomeScreen>{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    
+    return Scaffold( 
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 30.0),
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 120.0),
+              child: Text(
+                'What would you like to find?',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            
+            SizedBox(height: 20.0),
+            DestinationCarousal(),
+            
+          ],
+        ),
+      ), 
+
+    );
   }
   
 }
